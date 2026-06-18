ee57c7c0af28f7d849267b36135d8e4907bc674b
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-06-18 15:53:09 +0300

    adding note feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,23 @@ import {useNotes} from './store'
 
 const App = () => {
   const notes = useNotes()
+  const {add} = useNoteActions()
+
+  const generateId = () => Number((Math.random() * 1000000).toFixed(0))
+
+  const addNote = (e) => {
+    e.preventDefault()
+    const content = e.target.note.value
+    add({ id: generateId(), content, important: false })
+    e.target.reset()
+  }
 
   return (
     <div>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type='submit'>Add Note</button>
+      </form>
       <ul>
         {notes.map(note => (
           <li key={note.id}>
