ac1663826f266cf5259234a65d15996a9f5b9bee
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-07-01 16:17:15 +0300

    making changes

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -6,7 +6,7 @@ const Note = ({ note }) => {
     return (
         <li>
             {note.important ? <strong>{note.content}</strong> : note.content}
-            <button onclick={() => toggleImportance(note.id)}>
+            <button onClick={() => toggleImportance(note.id)}>
                 {note.important ? 'make not important' : 'make important'}
             </button>
         </li>
