28e6a13f476c3092f79a6a181f8ecc4054ef8524
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-07-02 14:48:11 +0300

    removing filter functionality

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -3,13 +3,6 @@ import Note from './Note'
 
 const NoteList = () => {
     const notes = useNotes()
-    const filter = useFilter()
-
-    const notesToShow = notes.filter(note => {
-        if (filter === 'important') return note.important
-        if (filter === 'nonimportant') return !note.important
-        return true
-    })
 
     return (
         <ul>
