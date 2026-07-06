bda23ebd66e3266e37da4b830d90d17b1b3e1278
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-07-06 14:36:05 +0300

    implementing intialization of notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import NoteList from './components/NoteList'
 import NoteForm from './components/NoteForm'
 import VisibilityFilter from './components/VisibilityFilter'
+import { useNoteActions } from './store'
+import noteService from './services/notes'
+import { useEffect } from 'react'
 
 const App = () => {
+  const { initialize } = useNoteActions()
+  
+  useEffect(() => {
+    noteService.getAll().then(notes => initialize(notes))
+  }, {initialize})
 
   return (
     <div>
