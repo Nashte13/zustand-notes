f3284cd8865fccb9d6c08f289c4de82e8f6f5cbf
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-07-06 14:28:45 +0300

    removing hard coded notes

--- src/store.js
+++ src/store.js
@@ -1,22 +1,5 @@
 import { create } from 'zustand'
 
-const initialNotes = [
-    {
-        id: 1,
-        content: 'Zustand is a state management library',
-        important: true
-    },
-    {
-        id: 2,
-        content: 'It is a small, fast and scalable bearbones state-management solution',
-        important: false
-    },
-    {
-        id: 3,
-        content: 'It has a minimal API and is easy to learn',
-        important: true
-    }
-]
 
 const useNoteStore = create(set => ({
     notes: initialNotes,
