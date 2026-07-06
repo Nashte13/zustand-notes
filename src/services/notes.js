e1768f27e934084c410b06748619c9d50b50bb58
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-07-06 15:37:57 +0300

    addding error handling

--- src/services/notes.js
+++ src/services/notes.js
@@ -2,7 +2,11 @@ const baseUrl = 'http://localhost:3001/notes'
 
 const getAll = async () => {
     const response = await fetch(baseUrl)
-    return response.json()
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch notes')
+    }
+    return await response.json()
 }
 
 export default { getAll }
